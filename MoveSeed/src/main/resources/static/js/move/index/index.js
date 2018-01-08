8594449f1d11e6e01b083f80bdf01dce9a2eeb08
Madong1994 <1170174224@qq.com>
2018-01-08 22:08:25 +0800

    界面布局完成

---
+++ MoveSeed/src/main/resources/static/js/move/index/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by 马东 on 2018/1/8.
+ */
+function initVue(){
+    var indexVue = new vue({
+        el:"#move_root",
+        data:{
+            moves:[],
+            pageSize:30,
+            page:1,
+            count:0
+        },
+        mounted:{
+
+        },
+        methods:{
+
+        }
+    })
+}
